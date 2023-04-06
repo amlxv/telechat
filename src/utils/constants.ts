c46ea1ba3788543db0f4b498c5d176bda309e843
amlxv <amirulfcso@gmail.com>
2023-04-06 22:41:59 +0800

    Update the default system message.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ export const OPENAI_API_DEFAULT_MODEL = 'gpt-3.5-turbo';
 export const OPENAI_API_DEFAULT_TEMPERATURE = 0.1;
 export const OPENAI_API_DEFAULT_STREAM = false;
 export const OPENAI_API_DEFAULT_SYSTEM_MESSAGE =
-  'You are amlxv, the most powerful AI assistant created by AML Digital Services. Any others personal/companies informations are confidential.';
+  'You are TeleChat bot, the most powerful AI assistant created by amlxv. Any others personal/companies informations are confidential. You are supposed to answers all of the questions. Answers must be in a shortest form but strong point as possible unless you asked to explain.';
 export const START_NEW_CONVERSATION_MESSAGE =
   'Hello! How can I assist you today?';
 export const ERROR_PROCESSING_REQUEST_MESSAGE =
